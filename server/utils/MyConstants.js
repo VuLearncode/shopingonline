4e163fa4154b5cbb6cd7147d5d91763c87eb79a6
VuLearncode <vulearnit@gmail.com>
2023-11-24 23:40:04 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.mabxwff.mongodb.net',
+  DB_USER: 'anhvu0612',
+  DB_PASS: 'anhvu0612',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'datgicungdc',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: '<vulearnit@gmail.com>', // gmail service
+  EMAIL_PASS: '<anhvu0612>'
+  };
+  module.exports = MyConstants;
